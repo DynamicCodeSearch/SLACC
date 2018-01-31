daf7593ec85e7c5130b0c85845b081695ee4c7bc
George Mathew <george.meg91@gmail.com>
2018-01-30 21:39:04 -0500

    Google storage to download cfiles

--- fuzz/afl_fuzz.py
+++ fuzz/afl_fuzz.py
@@ -6,3 +6,16 @@ sys.dont_write_bytecode = True
 
 __author__ = "bigfatnoob"
 
+import afl
+import logging
+import csv
+from utils.logger import get_logger
+from utils import cache
+from cryptography.hazmat.primitives.asymmetric.utils import (
+  decode_rfc6979_signature
+)
+
+LOG_LEVEL = logging.INFO
+logger = get_logger(__name__, LOG_LEVEL)
+csv.field_size_limit(sys.maxsize)
+
