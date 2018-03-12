4f65b755027578d99d9bdd592476f4f875627a27
George Mathew <george.meg91@gmail.com>
2018-03-11 21:35:13 -0400

    Wrong type of error

--- utils/google_storage.py
+++ utils/google_storage.py
@@ -21,6 +21,7 @@ from apiclient import discovery
 import httplib2
 from pydrive.auth import GoogleAuth, RefreshError
 from pydrive.drive import GoogleDrive
+from pydrive.files import ApiRequestError
 from utils import cache
 
 
@@ -223,7 +224,7 @@ def upload_file_to_drive(source, destination, folder_id=GDRIVE_FOLDER_ID):
     try:
       gfile.Upload()
       break
-    except RefreshError as e:
+    except ApiRequestError as e:
       cnt += 1
       time.sleep(5)
       logger.info("Failed to upload file: %s. Sleeping 5 seconds" % file_name)
