94571d610944b78604976f08af7155f96e5da122
George Mathew <george.meg91@gmail.com>
2018-03-11 21:15:24 -0400

    Upload error. Come on not again

--- utils/google_storage.py
+++ utils/google_storage.py
@@ -225,6 +225,8 @@ def upload_file_to_drive(source, destination, folder_id=GDRIVE_FOLDER_ID):
       break
     except RefreshError as e:
       cnt += 1
+      time.sleep(5)
+      logger.info("Failed to upload file: %s. Sleeping 5 seconds" % file_name)
       if cnt == 5:
         raise e
   logger.info("Uploaded file '%s' to google drive" % destination)
