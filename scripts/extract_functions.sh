0fbd3cbe5d03a7c7643e0c110537ca4e34a61c63
George Mathew <george.meg91@gmail.com>
2018-03-18 17:01:35 -0400

    Fix bug in scripts

--- scripts/extract_functions.sh
+++ scripts/extract_functions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name csExtractFunctions
-#SBATCH -N 1
+#SBATCH -N 8
 #SBATCH -p opteron
-#SBATCH -x c[79-98, 101-107]
+#SBATCH -x c[79-98,101-107]
 ### Use modules to set the software environment
 
-python c_utils/c_parser.py -n 1
+python c_utils/c_parser.py -n 32
--- scripts/uniform.sh
+++ scripts/uniform.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 16
 #####SBATCH -n 16
 #SBATCH -p opteron
-#SBATCH -x c[79-98, 101-107]
+#SBATCH -x c[79-98,101-107]
 # Use modules to set the software environment
 
 python fuzz/uniform.py 16
