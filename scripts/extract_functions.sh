0bc25b160b124aac814e7aca30df05111ac08e86
George Mathew <george.meg91@gmail.com>
2018-03-18 00:38:22 -0400

    Reducing memory for parallel create

--- scripts/extract_functions.sh
+++ scripts/extract_functions.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name csExtractFunctions
 #SBATCH -N 4
-#SBATCH -n 16
+#SBATCH -n 8
 #SBATCH -p opteron
 ### Use modules to set the software environment
 
-python c_utils/c_parser.py -n 16
+python c_utils/c_parser.py -n 8
