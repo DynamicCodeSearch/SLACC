deef90925922ab76f2aa57fba2ca28fae08c5335
George Mathew <george.meg91@gmail.com>
2018-08-25 16:07:44 -0400

    Error in JavaFormatter sorted

--- code/src/main/java/edu/ncsu/utils/JavaFormatter.java
+++ code/src/main/java/edu/ncsu/utils/JavaFormatter.java
@@ -6,16 +6,21 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 public class JavaFormatter {
 
+    private final static Logger LOGGER = Logger.getLogger(JavaFormatter.class.getName());
+
     public static String format(String fileName) {
+        String source = null;
         try {
-            String source = FileUtils.readFileToString(new File(fileName), StandardCharsets.UTF_8);
+            source = FileUtils.readFileToString(new File(fileName), StandardCharsets.UTF_8);
             Formatter formatter = new Formatter(new JavaFormatterOptions(JavaFormatterOptions.JavadocFormatter.ECLIPSE, JavaFormatterOptions.Style.GOOGLE, JavaFormatterOptions.SortImports.ALSO));
             return formatter.formatSource(source);
         } catch (Exception ex) {
-            throw new RuntimeException(ex);
+            LOGGER.severe("Format and Save Exception. " + ex.getMessage());
+            return source;
         }
     }
 
