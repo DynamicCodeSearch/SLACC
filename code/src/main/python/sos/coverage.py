ca7b2879a98839bb426c4c99c1b1e8ceb9668bb0
George Mathew <george.meg91@gmail.com>
2018-08-25 17:50:01 -0400

    Coverage for files

---
+++ code/src/main/python/sos/coverage.py
@@ -0,0 +1,73 @@
+from __future__ import print_function, division
+import sys
+import os
+
+sys.path.append(os.path.abspath("."))
+sys.dont_write_bytecode = True
+
+__author__ = "bigfatnoob"
+
+from utils import cache, stat, logger
+import properties
+
+import json
+import re
+
+
+LOGGER = logger.get_logger(os.path.basename(__file__.split(".")[0]))
+
+
+def coverage(project_name):
+  """
+  Return coverage for project.
+  :param project_name: Name of the project.
+  :return: Dictionary with project_name, sloc, lines_covered and code_coverage
+  """
+  coverage_file = "%s/%s/sloc.json" % (properties.META_RESULTS_FOLDER, project_name)
+  json_data = json.loads(cache.read_file(file_name=coverage_file))
+  sloc = 0
+  lines_covered = 0
+  for java_file, stats in json_data.items():
+    f_sloc = len(stats["sloc"])
+    f_lines_covered = len(stats["linesCovered"])
+    sloc += f_sloc
+    lines_covered += f_lines_covered
+  code_coverage = lines_covered / sloc * 100
+  return {
+    "project": project_name,
+    "sloc": sloc,
+    "lines_covered": lines_covered,
+    "code_coverage": code_coverage
+  }
+
+
+def _code_jam():
+  """
+  Run coverage for codejam
+  :return:
+  """
+  codejam_format = r'Y\d+R\d+P\d+'
+  total_coverage_percentages = []
+  reports = []
+  for project_name in sorted(cache.list_dir(properties.META_RESULTS_FOLDER, is_absolute=False)):
+    if re.match(codejam_format, project_name):
+      LOGGER.info("For project %s" % project_name)
+      project_path = "%s/%s" % (properties.META_RESULTS_FOLDER, project_name)
+      coverage_percentages = []
+      for user in cache.list_dir(project_path, is_absolute=False):
+        project_folder = os.path.join(project_name, user)
+        metrics = coverage(project_folder)
+        coverage_percentages.append(metrics['code_coverage'])
+      reports.append("\n### Project: %s" % project_name)
+      reports.append(stat.Stat(coverage_percentages).report())
+      total_coverage_percentages += coverage_percentages
+  LOGGER.info("Aggregating ...")
+  reports.append("\n### Total Coverage")
+  reports.append(stat.Stat(total_coverage_percentages).report())
+  report = "\n".join(reports)
+  cache.write_file(os.path.join(properties.RESULTS_FOLDER, "codejam", "coverage.md"), report)
+  return report
+
+
+if __name__ == "__main__":
+  _code_jam()
---
+++ code/src/main/python/utils/cache.py
@@ -0,0 +1,96 @@
+from __future__ import print_function, division
+import sys
+import os
+
+sys.path.append(os.path.abspath("."))
+sys.dont_write_bytecode = True
+
+__author__ = "bigfatnoob"
+
+import cPickle as cPkl
+
+
+def read_file(file_name):
+  """
+  Name of the file.
+  :param file_name:
+  :return:
+  """
+  with open(file_name) as f:
+    return f.read()
+
+
+def write_file(file_name, content):
+  """
+  Writing to the file
+  :param file_name: Name of the file
+  :param content: Content of the file
+  :return:
+  """
+  splits = file_name.rsplit(os.path.sep, 1)
+  if len(splits) > 1:
+    mkdir(splits[0])
+  with open(file_name, "wb") as f:
+    f.write(content)
+
+
+def load_pickle(file_name, verbose=False):
+  """
+  :return: Content of file_name
+  """
+  if not file_exists(file_name):
+    if verbose:
+      print("File %s does not exist" % file_name)
+    return None
+  try:
+    with open(file_name) as f:
+      return cPkl.load(f)
+  except Exception:
+    if verbose:
+      print("Exception while loading file" % file_name)
+    return None
+
+
+def save_pickle(file_name, obj):
+  """
+  Save obj in file_name
+  :param file_name:
+  :param obj:
+  :return:
+  """
+  splits = file_name.rsplit(os.path.sep, 1)
+  if len(splits) > 1:
+    mkdir(splits[0])
+  with open(file_name, "wb") as f:
+    cPkl.dump(obj, f, cPkl.HIGHEST_PROTOCOL)
+
+
+def file_exists(file_name):
+  """
+  Check if file or folder exists
+  :param file_name: Path of the file
+  :return: True/False
+  """
+  return os.path.exists(file_name)
+
+
+def mkdir(directory):
+  """
+  Create Directory if it does not exist
+  """
+  if not os.path.exists(directory):
+    try:
+      os.makedirs(directory)
+    except OSError as e:
+      if e.errno != os.errno.EEXIST:
+        raise
+
+
+def list_dir(directory, is_absolute=True):
+  """
+  List file names in directory
+  :param directory: Path of directory
+  :param is_absolute: If true returns complete path, else returns just the file/folder name
+  :return: List of file/folder names
+  """
+  return [os.path.join((directory, f)) if is_absolute else f for f in os.listdir(directory)]
