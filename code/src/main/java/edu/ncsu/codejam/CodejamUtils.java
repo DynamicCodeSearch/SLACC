054f15163d454f891ada9ca7cf601380d808b241
George Mathew <george.meg91@gmail.com>
2018-08-27 00:58:57 -0400

    Bug in setting constants

--- code/src/main/java/edu/ncsu/codejam/CodejamUtils.java
+++ code/src/main/java/edu/ncsu/codejam/CodejamUtils.java
@@ -19,7 +19,7 @@ public class CodejamUtils {
                 String userDir = Utils.pathJoin(problemDir, user);
                 for (String javaFile: Utils.listFilesWithExtension(userDir, ".java", true, true)) {
                     String fileName = Utils.getFileName(javaFile);
-                    if (!fileName.startsWith(Properties.GENERATED_CLASS_PREFIX))
+                    if (fileName.startsWith(Properties.GENERATED_CLASS_PREFIX))
                         generatedFiles.add(javaFile);
                 }
             }
