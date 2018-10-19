a85214ab7e6a20766fcc49a1bb7ffd24bb3738e5
George Mathew <george.meg91@gmail.com>
2018-10-18 20:03:04 -0400

    Snipping in parallel

---
+++ code/scripts/intro_class/snip_parallel.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sbatch scripts/intro_class/snip.sh checksum
+sbatch scripts/intro_class/snip.sh digits
+sbatch scripts/intro_class/snip.sh grade
+sbatch scripts/intro_class/snip.sh median
+sbatch scripts/intro_class/snip.sh smallest
+sbatch scripts/intro_class/snip.sh syllables
