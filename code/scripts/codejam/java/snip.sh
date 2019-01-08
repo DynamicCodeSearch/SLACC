aeb643a078f8f0f4918f1b2bdbade691c56716fc
George Mathew <george.meg91@gmail.com>
2019-01-08 12:51:50 -0500

    Snipping bug

--- code/scripts/codejam/java/snip.sh
+++ code/scripts/codejam/java/snip.sh
@@ -7,11 +7,11 @@
 
 if [ -z "$1" ]
 then
-    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/*/*/generated_class_*.java
-    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/*/*/temp_class_*.java
+    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/CodeJam/*/*/generated_class_*.java
+    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/CodeJam/*/*/temp_class_*.java
     java -jar target/code-1.0-SNAPSHOT-jar-with-dependencies.jar CodeJam snip
 else
-    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/$1/*/generated_class_*.java
-    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/$1/*/temp_class_*.java
+    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/CodeJam/$1/*/generated_class_*.java
+    rm ~/Raise/ProgramRepair/CodeSeer/projects/src/main/java/CodeJam/$1/*/temp_class_*.java
     java -jar target/code-1.0-SNAPSHOT-jar-with-dependencies.jar CodeJam snip $1
 fi
