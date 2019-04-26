8e79741577d72073ae87d2458afbf45e1893a7e5
George Mathew <george.meg91@gmail.com>
2019-04-26 19:50:14 -0400

    Parallelizing

--- code/scripts/misconceptions/compute_difference_parallel.sh
+++ code/scripts/misconceptions/compute_difference_parallel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sh scripts/misconceptions/compute_difference.sh 0 100
-sh scripts/misconceptions/compute_difference.sh 100 200
-sh scripts/misconceptions/compute_difference.sh 200 300
-sh scripts/misconceptions/compute_difference.sh 300 400
-sh scripts/misconceptions/compute_difference.sh 400 500
+sbatch scripts/misconceptions/compute_difference.sh 0 100
+sbatch scripts/misconceptions/compute_difference.sh 100 200
+sbatch scripts/misconceptions/compute_difference.sh 200 300
+sbatch scripts/misconceptions/compute_difference.sh 300 400
+sbatch scripts/misconceptions/compute_difference.sh 400 500
