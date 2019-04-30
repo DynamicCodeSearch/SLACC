e09723f4e14ab0dcf3f7c78467b89c5f62105a92
George Mathew <george.meg91@gmail.com>
2019-04-30 08:38:13 -0400

    Syntactic distances script

---
+++ code/scripts/misconceptions/syntactic.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name syntactic
+#SBATCH -N 1
+#SBATCH -p opteron
+# Use modules to set the software environment
+
+cd src/main/python
+python misconceptions/common/syntactic.py
