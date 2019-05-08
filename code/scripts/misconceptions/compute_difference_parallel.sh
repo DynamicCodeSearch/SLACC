3d4e3a33f1d88cbaaf5efa51e68767d700d2ee01
George Mathew <george.meg91@gmail.com>
2019-05-08 17:08:11 -0400

    Parallel scripts

--- code/scripts/misconceptions/compute_difference_parallel.sh
+++ code/scripts/misconceptions/compute_difference_parallel.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-sbatch scripts/misconceptions/compute_difference.sh 0 150
-sbatch scripts/misconceptions/compute_difference.sh 150 300
-sbatch scripts/misconceptions/compute_difference.sh 300 450
-sbatch scripts/misconceptions/compute_difference.sh 450 600
-sbatch scripts/misconceptions/compute_difference.sh 600 750
-sbatch scripts/misconceptions/compute_difference.sh 750 900
-sbatch scripts/misconceptions/compute_difference.sh 900 1050
-sbatch scripts/misconceptions/compute_difference.sh 1050 1200
-sbatch scripts/misconceptions/compute_difference.sh 1200 1350
-sbatch scripts/misconceptions/compute_difference.sh 1350 1500
-sbatch scripts/misconceptions/compute_difference.sh 1500 1700
+sbatch scripts/misconceptions/compute_difference.sh 0 100
+sbatch scripts/misconceptions/compute_difference.sh 100 200
+sbatch scripts/misconceptions/compute_difference.sh 200 300
+sbatch scripts/misconceptions/compute_difference.sh 300 400
+sbatch scripts/misconceptions/compute_difference.sh 400 500
+sbatch scripts/misconceptions/compute_difference.sh 500 600
+sbatch scripts/misconceptions/compute_difference.sh 600 700
+sbatch scripts/misconceptions/compute_difference.sh 700 800
+sbatch scripts/misconceptions/compute_difference.sh 800 900
+sbatch scripts/misconceptions/compute_difference.sh 900 1000
+sbatch scripts/misconceptions/compute_difference.sh 1000 1100
