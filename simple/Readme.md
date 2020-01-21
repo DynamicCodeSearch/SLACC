5d9ebfc4f180f1e9a62baa17e36384b450d14983
George Mathew <george.meg91@gmail.com>
2020-01-20 21:48:08 -0500

    Removing older projects

--- simple/Readme.md
+++
@@ -1,3 +0,0 @@
-## Projects 
-* Stores <language> Projects in respective src/main/<language> 
-* To make the projects folder run `mvn clean install`
--- simple/src/main/python/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function, division
-import sys
-import os
-
-sys.path.append(os.path.abspath("."))
-sys.dont_write_bytecode = True
-
-__author__ = "bigfatnoob"
-
